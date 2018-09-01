d1c6c88adae5b1aeb7a1d61204a31ed564121779
groiq <35578027+groiq@users.noreply.github.com>
2018-09-01 16:30:41 +0200

    initialized index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHPlayground</title>
+  </head>
+  <body>
+  <?php
+  
+  ?>
+  </body>
+</html>
