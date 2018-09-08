83a72946e4a248a097cdb2d6fd834434eb87b4dd
groiq <35578027+groiq@users.noreply.github.com>
2018-09-08 20:07:57 +0200

    permutation generator: finished version 1

--- index.php
+++ index.php
@@ -21,35 +21,59 @@
   function permutations($elements) {
       
       $allPermutations = array();
-      print_r($allPermutations);
-      say("list of permutations is initialized empty.");
+      // print_r($allPermutations);
+      // say("list of permutations is initialized empty.");
       
       $elemIsUsed = array();
       foreach($elements as $position) {
           $elemIsUsed[$position] = 0;
       }
-      print_r($elemIsUsed);
-      say("make an array to track whether an element is already assigned to a position.");
+      // print_r($elemIsUsed);
+      // say("make an array to track whether an element is already assigned to a position.");
       
       $permutationBlueprint = array();
       for ($i=0; $i < sizeof($elements); $i++) {
           $permutationBlueprint[] = 0;
       }
-      print_r($permutationBlueprint);
-      say("generate a permutation blueprint; list with a slot for each element, each slot is set to a placeholder");
+      // print_r($permutationBlueprint);
+      // say("generate a permutation blueprint; list with a slot for each element, each slot is set to a placeholder");
       
-      say("I need two lists - one generates the current permutation - the other tracks which elements are already in use --");
       
-      $allValuesForPos = function($pos) use($elements,$permutationBlueprint,&$allValuesForPos,&$allPermutations) {
-          say("fill pos $pos with each value in succession");
+      
+      $allValuesForPos = function($pos) use(&$elements,&$permutationBlueprint,&$elemIsUsed,&$allValuesForPos,&$allPermutations) {
+          // say("fill pos $pos with each value in succession");
           
           foreach($elements as $elemIndex => $element) {
+              if ($elemIsUsed[$element]==0) {
+                  // say("loop: $elemIndex as $element");
+                  $elemIsUsed[$element] = 1;
+                  // print_r($elemIsUsed);
+                  // say("element now marked as used");
+                  $permutationBlueprint[$pos] = $element;
+                  // print_r($permutationBlueprint);
+                  // say("slot element $element into current permutation at $pos");
+                  if ($pos < (sizeof($elements)-1)) {
+                      // say("not yet at last position, so recurse");
+                      $allValuesForPos($pos+1);
+                  } else {
+                      // print_r($permutationBlueprint);
+                      $allPermutations[] = $permutationBlueprint;
+                      // print_r($allPermutations);
+                      // say("at last position, so add blueprint to list of permutations");
+                      // foreach($permutationBlueprint as $slot => $bla) {
+                          // $permutationBlueprint[$slot] = 0;
+                      // }
+                      // say("empty permutation blueprint, just to be clean (actually unnecessary!)");
+                  }
+                  $elemIsUsed[$element] = 0;
+              }
               
           }
           
       };
       
       $allValuesForPos(0);
+      // print_r($allPermutations);
       
       
       
@@ -68,7 +92,7 @@
   // print_r($elements);
   // say("permutation list:");
   $permutationList = permutations($elements);
-  // print_r($permutationList);
+  print_r($permutationList);
   
   
   echo("</pre>");
