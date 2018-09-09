dabb54024666625fef753fff7164e5f8110ef8a4
groiq <35578027+groiq@users.noreply.github.com>
2018-09-09 08:19:15 +0200

    Guest List Calculator: setup

---
+++ partyguest.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Party guest list calculator</title>
+  </head>
+  <body>
+  
+  <h1>Party Gues List Calculator</h1>
+  
+  <p>
+  
+  </p>
+  
+  
+  </body>
+</html>
